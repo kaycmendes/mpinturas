324b4db37e2752f0a4150bfe7019f066d9f465df
Kayc <kaycmendes@gmail.com>
2020-09-03 13:58:15 -0300

    add google maps

--- js/jscript.js
+++ js/jscript.js
@@ -1,11 +1,16 @@
-
-
-
-  $(window).on('load', function() {
-    $(".slide").slick({
-        autoPlay: true,
-        autoplaySpeed: 1000,
-        draggable: true,
-        dots: true
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = { lat: -19.9167, lng: -43.9345 };
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+    document.getElementById('map'), { zoom: 11, center: uluru,  disableDefaultUI: true
     });
- });
+
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({
+    position: uluru, map: map,
+    icon: { path: google.maps.SymbolPath.CIRCLE, scale: 177, strokeColor: 'black', fillColor: 'yellow', strokeWeight: 1, fillOpacity: 0.5 }
+  });
+}
+
